5ba5d20aa3d5c5252fdb5a9a6ab0eca4ee071d66
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-01-21 12:16:21 -0800

    Made audio packet handler func

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 # Library imports
 import socket
 import sys
+import threading
 
 
 # Server details
@@ -13,6 +14,14 @@ if len(sys.argv) == 3:
 else:
 	print("Using default server address")
 
+
+# Channels that users can connect to
+channels = {}
+
+# User connections
+connections = {}
+
+
 # Send data to channel
 def send_channel(ch, uid, sock, b):
 	connected = ch[uid]
@@ -20,14 +29,45 @@ def send_channel(ch, uid, sock, b):
 	for addr in connected:
 		sock.sendto(b, addr)
 
+# Packet handler
+def handler(pack, addr, sock):
+	# Handle connection
+	if pack[0] == 0:
+		# Connect to channel
+		# Channel id
+		channel_id = pack[1:]
+
+		if channel_id in channels.keys():
+			# Connect to existing channel
+			channels[channel_id].append(addr)
+		else:
+			# Create new channel
+			channels[channel_id] = [addr]
+
+		# Add connection
+		connections[addr] = channel_id
+
+		# Verify connect packet
+		sock.sendto(b"\x00", addr)
+
+		print("User {}:{} joined on {}".format(addr[0], addr[1], channel_id))
+	elif pack[0] == 1:
+		# Leave channel
+		try:
+			channels[connections[addr]].remove(addr)
+			del connections[addr]
+
+			print("User {}:{} left.".format(addr[0], addr[1]))
+		except ValueError:
+			print("Invalid leave request from {}:{}".format(addr[0], addr[1]))
+	elif pack[0] == 2:
+		# Update video frame
+		# Send to channel
+		send_channel(channels, connections[addr], sock, pack[1:])
+
+
 # Server loop
 if __name__ == "__main__":
-	# Channels that users can connect to
-	channels = {}
-
-	# User connections
-	connections = {}
-
 	# Startup server
 	print("Starting up server...")
 	# Open UDP socket
@@ -42,39 +82,10 @@ if __name__ == "__main__":
 			# Recv packet
 			pack, addr = sock.recvfrom(1025)
 
-			# Handle connection
-			if pack[0] == 0:
-				# Connect to channel
-				# Channel id
-				channel_id = pack[1:]
-
-				if channel_id in channels.keys():
-					# Connect to existing channel
-					channels[channel_id].append(addr)
-				else:
-					# Create new channel
-					channels[channel_id] = [addr]
-
-				# Add connection
-				connections[addr] = channel_id
-
-				# Verify connect packet
-				sock.sendto(b"\x00", addr)
-
-				print("User {}:{} joined on {}".format(addr[0], addr[1], channel_id))
-			elif pack[0] == 1:
-				# Leave channel
-				try:
-					channels[connections[addr]].remove(addr)
-					del connections[addr]
-
-					print("User {}:{} left.".format(addr[0], addr[1]))
-				except ValueError:
-					print("Invalid leave request from {}:{}".format(addr[0], addr[1]))
-			elif pack[0] == 2:
-				# Update video frame
-				# Send to channel
-				send_channel(channels, connections[addr], sock, pack[1:])
+			# Run handler thread
+			handler_thread = threading.Thread(target=handler, args=(pack, addr, sock))
+			handler_thread.daemon = True
+			handler_thread.start()
 	except KeyboardInterrupt:
 		pass
 
